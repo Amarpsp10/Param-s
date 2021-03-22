b32e9a6af4c2f3cc31338251af36b02cb8a62485
amarpsp10 <amarpsp10@gmail.com>
2021-03-22 11:06:50 -0700

    nav bar resopsive

--- src/components/intro-section/Intro.js
+++ src/components/intro-section/Intro.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {motion} from 'framer-motion'
 import My_image from '../../images/my_image.png'
 import Param_Image from '../../images/param-image-left.jpeg'
+import Param_profile from '../../images/param-profile.png'
 import './Intro.css'
 
 class Intro extends Component {
@@ -9,28 +10,30 @@ class Intro extends Component {
 
     render() {
         return(
-            <div>
+            <div className={'div'}>
             
             <section  id="intro" className="intro">
-             
                <motion.div
                       initial={{x:-500}}
                       animate={{opacity:1,x:0, rotate:360}}s
                       transition={{duration:1,}}
-               
-               className="profile-image hideme">
-                 <img src={My_image} width="330px" alt="here is my profile photo" />
+                      
+                      className="profile-image hideme">
+                 <img src={Param_profile} width="350px" alt="here is my profile photo" />
                </motion.div>
       
                <div className="intro-text">
-                 <motion.h3 className='hi'
+                 <motion.h6 className='hi'
                     initial={{opacity:0,x:-108,}}
                     animate={{opacity:1,x:0,}}
-                    transition={{duration:2,repeat:Infinity}}
-                 >Hi</motion.h3>
+                    transition={{duration:2,}}
+                    >Hi...
+                 </motion.h6>
                  <h1>Amar Preet here!</h1>
-                 <p>dskl fksdjfkldjsk dskjfd djfkdjakldio sdjsdiojsd</p>
+                 <p>dskl fksdjfkldjsk dskjfd djfkdjakldio sdjsdiojsd sdlka dsdkl sd sdf sd fsdaf ds gsd gd sagfas glsdhjkafhjkashdkf sdkjfhkdsj dskljfkla lfkasdjklfjaskl klsdjflk </p>
+                 <button className={'button-resume'}>DOWNLOAD RESUME</button>
                </div>
+            
       
             </section>
 
--- src/components/mail-section/Form.js
+++ src/components/mail-section/Form.js
@@ -10,7 +10,7 @@ export default class form extends Component{
                 <h1 className={'heading'}>Contact Me</h1>
 
                 <div className='inputs'>
-                <input className={'input-name'} autoFocus='hidden' placeholder={'xyx123@gmail.com'} minlength="1" maxlength="50" type="text" id="name" name="Mail"/>
+                <input className={'input-name'} placeholder={'xyx123@gmail.com'} minlength="1" maxlength="50" type="text" id="name" name="Mail"/>
                 <input className={'input-name'} placeholder={'Enter your good Name'} minlength="1" maxlength="20" type="text" id="name" name="Name"/>
                 <input className={'input-name'} placeholder={'Subject'} minLength="0" maxLength="80" type="text" id="name" name="Subject"/>
                 <textarea className={'input-name message'} name="text" placeholder="Message :)"></textarea>
