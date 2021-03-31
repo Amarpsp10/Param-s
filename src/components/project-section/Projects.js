2878a272fecf21665abb5495d7cb3a2b43afd4d6
amarpsp10 <amarpsp10@gmail.com>
2021-03-31 10:32:01 -0700

    project section improved

--- src/components/project-section/Projects.js
+++ src/components/project-section/Projects.js
@@ -4,7 +4,10 @@ import './Projects.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import backimg from '../../images/building.jpg'
+import backimg1 from '../../images/splash1.jpg'
+import backimg2 from '../../images/splash2.jpg'
+import backimg3 from '../../images/splash3.jpg'
+
 export default class projects extends Component{
 
 
@@ -15,15 +18,15 @@ export default class projects extends Component{
                
               <Carousel infiniteLoop={true} emulateTouch={true} autoPlay={true} interval={4000} showThumbs={false} className={'slider'}>
               <div>
-                    <img src={backimg} className={'img'}/>
+                    <img src={backimg1} className={'img'}/>
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src={backimg} className={'img'}/>
+                    <img src={backimg2} className={'img'}/>
                     <p className="legend">Legend 2</p>
                 </div>
                 <div>
-                    <img src={backimg} className={'img'}/>
+                    <img src={backimg3} className={'img'}/>
                     <p className="legend">Legend 3</p>
                 </div>
               </Carousel>
